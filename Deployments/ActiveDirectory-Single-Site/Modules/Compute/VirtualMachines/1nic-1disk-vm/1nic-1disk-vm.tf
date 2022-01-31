09facf211ac5ea1640fe5147a84110ddd79da9f3
github@mcitpnetworks.com <github@mcitpnetworks.com>
2022-01-31 02:01:23 +0000

    Iniial Deployment

---
+++ Deployments/ActiveDirectory-Single-Site/Modules/Compute/VirtualMachines/1nic-1disk-vm/1nic-1disk-vm.tf
@@ -0,0 +1,118 @@
+variable "ResourceGroupName" {
+  type        = string
+  description = "Resource Group Name"
+}
+
+variable "computerName" {
+  type        = string
+  description = "Computer Name"
+}
+
+variable "computerIP" {
+  type        = string
+  description = "Computer IP"
+}
+
+variable "Publisher" {
+  type        = string
+  description = "Image Publisher"
+}
+
+variable "Offer" {
+  type        = string
+  description = "Image Publisher Offer"
+}
+
+variable "OSVersion" {
+  type        = string
+  description = "OS Version"
+}
+
+variable "licenseType" {
+  type        = string
+  description = "License Type (Windows_Server or None)"
+}
+
+variable "DataDisk1Name" {
+  type        = string
+  description = "Name of Data Disk 1"
+}
+
+variable "Location" {
+  type        = string
+  description = "Resource Location"
+}
+
+variable "subnet" {
+  type        = string
+  description = "Subnet Name"
+}
+
+variable "vmsize" {
+  type        = string
+  description = "VM Size"
+}
+
+variable "adminUsername" {
+  type        = string
+  description = "Admin User Name"
+}
+
+variable "adminPassword" {
+  type        = string
+  sensitive   = true
+  description = "Admin User Name"
+}
+
+locals {
+  NicName             = "${var.computerName}-nic"
+  StorageAccountType  = "Standard_LRS"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = local.NicName
+  location            = var.Location
+  resource_group_name = var.ResourceGroupName
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = var.subnet
+    private_ip_address            = var.computerIP
+    private_ip_address_allocation = "Static"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.computerName
+  location              = var.Location
+  resource_group_name   = var.ResourceGroupName
+  vm_size               = var.vmsize
+  license_type          = var.licenseType
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  storage_image_reference {
+    publisher = var.Publisher
+    offer     = var.Offer
+    sku       = var.OSVersion
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${var.computerName}_OSDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = local.StorageAccountType
+  }      
+
+  os_profile {
+    computer_name  = var.computerName
+    admin_username = var.adminUsername
+    admin_password = var.adminPassword
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent  = true
+  }
+}
---
+++ Deployments/ActiveDirectory-Single-Site/main.tf
@@ -0,0 +1,213 @@
+provider "azurerm" {
+  skip_provider_registration = true
+  features {
+  }
+}
+
+variable "ResourceGroupName1" {
+  type        = string
+  description = "Resource Group 1 Name"
+}
+
+variable "TimeZone1" {
+  type        = string
+  description = "Time Zone or Region 1"
+}
+
+variable "adminUsername" {
+  type        = string
+  description = "Admin User Name"
+}
+
+variable "adminPasswordName" {
+  type        = string
+  sensitive   = true
+  description = "Naming Convention for All Resources"
+}
+
+variable "WindowsServerLicenseType" {
+  type        = string
+  description = "Windows Server OS License Type"
+}
+
+variable "WindowsClientLicenseType" {
+  type        = string
+  description = "Windows Client OS License Type"
+}
+
+variable "NamingConvention" {
+  type        = string
+  description = "Naming Convention for All Resources"
+}
+
+variable "SubDNSDomain" {
+  type        = string
+  description = "Sub DNS Domain Name Example:  sub1. must include a DOT AT END"
+}
+
+variable "SubDNSBaseDN" {
+  type        = string
+  description = "Sub DNS BaseDN Example:  DC=sub2,DC=sub1, must include a DOT AT COMMA"
+}
+
+variable "NetBiosDomain" {
+  type        = string
+  description = "NetBios Domain Name"
+}
+
+variable "InternalDomain" {
+  type        = string
+  description = "Second-Level Domain"
+}
+
+variable "InternalTLD" {
+  type        = string
+  description = "Top-Level Domain"
+}
+
+variable "Location1" {
+  type        = string
+  description = "Resource Location"
+}
+
+variable "vnet1ID" {
+  type        = string
+  description = "VNet1 1st 2 Octets"
+}
+
+variable "DC1OSVersion" {
+  type        = string
+  description = "Domain Controller 1 VM Size"
+}
+
+variable "dc1vmsize" {
+  type        = string
+  description = "Domain Controller 1 VM Size"
+}
+
+variable "artifactslocation" {
+  type        = string
+  description = "The location of resources, such as templates and DSC modules, that the template depends on"
+}
+
+variable "artifactslocationSasToken" {
+  type        = string
+  sensitive   = true
+  description = "The location of resources, such as templates and DSC modules, that the template depends on"
+}
+
+variable "KeyVaultName" {
+  type        = string
+  description = "Key Vault Name"
+}
+
+variable "KeyVaultResourceId" {
+  type        = string
+  description = "Key Vault Resource Id"
+}
+
+locals {
+  StorageAccountType  =   "Standard_LRS"
+  VNet1               =   {
+
+    vnet1Name                 = "${var.NamingConvention}-VNet1"
+    vnet1Prefix               = "${var.vnet1ID}.0.0/16"
+    vnet1subnet1Name          = "${var.NamingConvention}-VNet1-Subnet1"
+    vnet1subnet1Prefix        = "${var.vnet1ID}.1.0/24"
+    vnet1subnet2Name          = "${var.NamingConvention}-VNet1-Subnet2"
+    vnet1subnet2Prefix        = "${var.vnet1ID}.2.0/24"    
+    vnet1BastionsubnetPrefix  = "${var.vnet1ID}.253.0/24"        
+
+  }
+  dc1name                     =   "${var.NamingConvention}-dc-01"
+  dc1IP                       =   "${var.vnet1ID}.1.101"
+  DataDisk1Name               =   "NTDS"
+  InternalDomainName          =   concat("${var.SubDNSDomain},${var.InternalDomain}","${var.InternalTLD}")
+}
+
+data "azurerm_key_vault_secret" "main" {
+  name         = var.adminPasswordName
+  key_vault_id = var.KeyVaultResourceId
+}
+
+resource "random_string" "random" {
+  length  = 8
+  special = false
+}
+
+resource "azurerm_resource_group" "RG1" {
+  name     = var.ResourceGroupName1
+  location = var.Location1
+}
+
+resource "azurerm_storage_account" "STORAGE1" {
+  name                     = "singlestorage${lower(random_string.random.result)}"
+  location                 = var.Location1
+  resource_group_name      = var.ResourceGroupName1
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  depends_on = [
+    azurerm_resource_group.RG1,
+    random_string.random
+  ]
+}
+
+module "deployVNet1" {
+  source   = "./Modules/Network/VirtualNetwork"
+  vnetName            = local.VNet1.vnet1Name
+  vnetPrefix          = local.VNet1.vnet1Prefix
+  subnet1Name         = local.VNet1.vnet1subnet1Name
+  subnet1Prefix       = local.VNet1.vnet1subnet1Prefix
+  subnet2Name         = local.VNet1.vnet1subnet2Name
+  subnet2Prefix       = local.VNet1.vnet1subnet2Prefix  
+  BastionsubnetPrefix = local.VNet1.vnet1BastionsubnetPrefix
+  Location            = var.Location1
+  ResourceGroupName   = var.ResourceGroupName1
+  depends_on = [
+    azurerm_resource_group.RG1
+  ]
+}
+
+module "deployDC1" {
+  source   = "./Modules/Compute/VirtualMachines/1nic-2disk-vm"
+  computerName        = local.dc1name
+  computerIP          = local.dc1IP
+  Publisher           = "MicrosoftWindowsServer"
+  Offer               = "WindowsServer"
+  OSVersion           = var.DC1OSVersion    
+  licenseType         = var.WindowsServerLicenseType
+  DataDisk1Name       = local.DataDisk1Name
+  vmsize              = var.dc1vmsize  
+  Location            = var.Location1
+  ResourceGroupName   = var.ResourceGroupName1
+  subnet              = module.deployVNet1.subnet1ID
+  adminUsername       = var.adminUsername
+  adminPassword       = data.azurerm_key_vault_secret.main.value
+  depends_on = [
+    azurerm_resource_group.RG1,
+    module.deployVNet1
+  ]
+
+}
+
+module "PromoteDC1" {
+  source   = "./Modules/Compute/VirtualMachines/1nic-2disk-vm"
+  computerName        = local.dc1name
+  TimeZone            = var.TimeZone1
+  NetBiosDomain       = var.NetBiosDomain
+  domainName          = local.InternalDomainName
+  Location            = var.Location1
+  adminUsername       = var.adminUsername
+  adminPassword       = data.azurerm_key_vault_secret.main.value  
+  artifactsLocation   = var.dc1vmsize  
+  depends_on = [
+    azurerm_resource_group.RG1,
+    module.deployDC1
+  ]
+
+}
+
+output "adminUserName" {
+  value = var.adminUsername
+}
