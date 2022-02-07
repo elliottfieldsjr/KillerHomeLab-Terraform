dc17a0fc7778601db1f94ea464c0032ef9a82423
github@mcitpnetworks.com <github@mcitpnetworks.com>
2022-02-07 08:46:42 -0500

    Change Infrastructure

--- Deployments/ActiveDirectory-Single-Site/RestartDC1/restartvm.tf
+++ Deployments/ActiveDirectory-Single-Site/RestartDC1/restartvm.tf
@@ -49,15 +49,8 @@ data "azurerm_virtual_machine" "dc1" {
   resource_group_name = var.ResourceGroupName1
 }
 
-data "azurerm_virtual_machine_extension" "dsc" {
-  name               = "Microsoft.Powershell.DSC"
-  virtual_machine_id = data.azurerm_virtual_machine.dc1.id
-
-}
-
-
 resource "azurerm_virtual_machine_extension" "dsc" {
-  name                       = "Microsoft.Powershell.DSC"
+  name                       = "Microsoft.Powershell.DSC1"
   virtual_machine_id         = data.azurerm_virtual_machine.dc1.id
   publisher                  = "Microsoft.Powershell"
   type                       = "DSC"
